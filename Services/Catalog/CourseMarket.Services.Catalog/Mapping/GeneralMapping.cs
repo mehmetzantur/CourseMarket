cd7ac6fc11cb408c5ae8240cdf17b6bce9f2932d
mehmetzantur <mehmetzantur@gmail.com>
2025-06-09 18:11:37 +0300

    added AutoMapper library

---
+++ Services/Catalog/CourseMarket.Services.Catalog/Mapping/GeneralMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using CourseMarket.Services.Catalog.Dtos;
+using CourseMarket.Services.Catalog.Models;
+
+namespace CourseMarket.Services.Catalog.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course, CourseDto>().ReverseMap();
+            CreateMap<Course, CourseUpdateDto>().ReverseMap();
+            CreateMap<Course, CourseCreateDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Feature, FeatureDto>().ReverseMap();
+        }
+    }
+}
