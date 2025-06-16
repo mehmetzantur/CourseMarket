b780e980af2bc5e2bc8686fcdffe65d4bc3f05f7
mehmetzantur <mehmetzantur@gmail.com>
2025-06-16 14:40:39 +0300

    Created CategoriesController

---
+++ Services/Catalog/CourseMarket.Services.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,41 @@
+﻿using CourseMarket.Services.Catalog.Dtos;
+using CourseMarket.Services.Catalog.Services;
+using CourseMarket.Shared.ControllerBases;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CategoriesController : CustomControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return CreateActionResultInstance(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var category = await _categoryService.GetByIdAsync(id);
+            return CreateActionResultInstance(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var response = await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(response);
+        }
+    }
+}
