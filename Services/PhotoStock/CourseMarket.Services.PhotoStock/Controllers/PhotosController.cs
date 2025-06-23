057fac5f82599e0dc2653ae17de2445412389d49
mehmetzantur <mehmetzantur@gmail.com>
2025-06-23 17:33:18 +0300

    Add photo upload endpoint and PhotoDto

---
+++ Services/PhotoStock/CourseMarket.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,33 @@
+﻿using CourseMarket.Services.PhotoStock.Dtos;
+using CourseMarket.Shared.ControllerBases;
+using CourseMarket.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken) 
+        {
+            if(photo is not null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                
+                var returnPath = "photos/" + photo.FileName;
+                PhotoDto photoDto = new() { Url = returnPath };
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty!", 400));
+        }
+    }
+}
