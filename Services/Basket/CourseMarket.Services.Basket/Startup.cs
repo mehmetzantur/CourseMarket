fbcaa82985c7b64f68965c4ff6e4466023b92be8
mehmetzantur <mehmetzantur@gmail.com>
2025-06-24 12:19:06 +0300

    Configured Redis settings

--- Services/Basket/CourseMarket.Services.Basket/Startup.cs
+++ Services/Basket/CourseMarket.Services.Basket/Startup.cs
@@ -1,3 +1,4 @@
+using CourseMarket.Services.Basket.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -25,7 +26,7 @@ namespace CourseMarket.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
