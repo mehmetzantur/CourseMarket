99194234152fafddbce870e5e8674b0d4e013762
mehmetzantur <mehmetzantur@gmail.com>
2025-06-24 12:56:18 +0300

    Added SharedIdentityService for getting user id in token

--- Services/Basket/CourseMarket.Services.Basket/Startup.cs
+++ Services/Basket/CourseMarket.Services.Basket/Startup.cs
@@ -1,5 +1,6 @@
 using CourseMarket.Services.Basket.Services;
 using CourseMarket.Services.Basket.Settings;
+using CourseMarket.Shared.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,8 @@ namespace CourseMarket.Services.Basket
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
             services.Configure<RedisSettings>(Configuration.GetSection("RedisSettings"));
             services.AddSingleton<RedisService>(sp =>
             {
