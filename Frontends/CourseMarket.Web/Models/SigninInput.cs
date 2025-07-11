2e72b4eca47a280eb1f8d13ada3d112843041678
mehmetzantur <mehmetzantur@gmail.com>
2025-07-11 18:32:12 +0300

    Added abstract methods for sign in token operations

---
+++ Frontends/CourseMarket.Web/Models/SigninInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseMarket.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Remember Me")]
+        public bool IsRemember { get; set; }
+    }
+}
---
+++ Frontends/CourseMarket.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,14 @@
+﻿using CourseMarket.Shared.Dtos;
+using CourseMarket.Web.Models;
+using Duende.IdentityModel.Client;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Web.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+        Task<TokenResponse> GetAccessTokenByRefleshToken();
+        Task RevokeRefleshToken();
+    }
+}
