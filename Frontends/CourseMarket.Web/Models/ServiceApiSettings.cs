7ba049a69d0c34089ad7d3ffb7345dcd5e93b8d6
mehmetzantur <mehmetzantur@gmail.com>
2025-07-11 18:46:58 +0300

    Added settings classes for appsettings sections via OptionPattern

---
+++ Frontends/CourseMarket.Web/Models/ServiceApiSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseMarket.Web.Models
+{
+    public class ServiceApiSettings
+    {
+        public string BaseUri { get; set; }
+        public string PhotoStockUri { get; set; }
+    }
+}
