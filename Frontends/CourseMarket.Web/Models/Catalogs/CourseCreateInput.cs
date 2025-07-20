8def9b89282bdf80df620d26f4dd38cde93fdcf5
mehmetzantur <mehmetzantur@gmail.com>
2025-07-20 17:57:06 +0300

    Added Course crud operations without posting image

--- Frontends/CourseMarket.Web/Models/Catalogs/CourseCreateInput.cs
+++ Frontends/CourseMarket.Web/Models/Catalogs/CourseCreateInput.cs
@@ -1,15 +1,21 @@
-﻿namespace CourseMarket.Web.Models.Catalogs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CourseMarket.Web.Models.Catalogs
 {
     public class CourseCreateInput
     {
         public string UserId { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Display(Name = "Category")]
         public string CategoryId { get; set; }
 
         public string Picture { get; set; }
