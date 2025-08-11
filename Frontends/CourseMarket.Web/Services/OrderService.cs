15ac35b82ed2bc03bfb4cafc1ab657e842f8b2d7
mehmetzantur <mehmetzantur@gmail.com>
2025-08-11 17:03:07 +0300

    order service implementation

---
+++ Frontends/CourseMarket.Web/Services/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using CourseMarket.Web.Models.Orders;
+using CourseMarket.Web.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CourseMarket.Web.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Task<OrderCreatedViewModel> CreateOrder(CheckoutInfoInput checkoutInfoInput)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<OrderViewModel>> GetOrder()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task SuspendOrder(CheckoutInfoInput checkoutInfoInput)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
