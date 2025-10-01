f80822d63aced160bcd128442558f126d28d0107
mehmetzantur <mehmetzantur@gmail.com>
2025-10-01 18:04:45 +0300

    added method type for photo delete

--- Services/PhotoStock/CourseMarket.Services.PhotoStock/Controllers/PhotosController.cs
+++ Services/PhotoStock/CourseMarket.Services.PhotoStock/Controllers/PhotosController.cs
@@ -14,14 +14,14 @@ namespace CourseMarket.Services.PhotoStock.Controllers
     public class PhotosController : CustomControllerBase
     {
         [HttpPost]
-        public async Task<IActionResult> Save(IFormFile photo, CancellationToken cancellationToken) 
+        public async Task<IActionResult> Save(IFormFile photo, CancellationToken cancellationToken)
         {
             if(photo is not null && photo.Length > 0)
             {
                 var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
                 using var stream = new FileStream(path, FileMode.Create);
                 await photo.CopyToAsync(stream, cancellationToken);
-                
+
                 var returnPath = photo.FileName;
                 PhotoDto photoDto = new() { Url = returnPath };
                 return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
@@ -30,6 +30,7 @@ namespace CourseMarket.Services.PhotoStock.Controllers
             return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty!", 400));
         }
 
+        [HttpDelete]
         public IActionResult Delete(string photoUrl)
         {
             var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
